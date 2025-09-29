c54088ffff1fa2b8f5ee05c471a42dd4efabe46b
Harsh M <harshmaroo123@gmail.com>
2025-09-29 19:54:41 +0530

    updated todos

--- todo.js
+++ todo.js
@@ -1,12 +1,57 @@
-// - secure firebase firestore rules
-// - Create admin UI for uploading products
+/////////////////////////////////////
+// admin panel
+// - generate UI from claude based on the final product page UI
+// - Create admin UI for uploading/searching/editing products
+// - Create admin UI for uploading brands
+// - compress images before uploading
+
+//////////////////////////////
+// - add items in model:
+//   - inventory/stock count
+//   - save discounted price in model
+//   - isFeatured boolean for home page products / highest rated products on homepage
 
 
 //////////////////////////////
-/*
+// current todo
+// - price range filter fix
+// - filters fixes:
+//   - reset filters button
+//   - remove the current category/subcategory text from above filters
+//   - when on products page, show all brands and a page text link type ui for the categories
+// - bottomnav links add
+// - update current product details UI with support for image listview/multiple images
+// - use discounted price for price based sort/filter once added in model
 
-HOme page:
+
+//////////////////////////////
+// Home page:
+/*
 carousels with shop button for
 top bikes
 top accesories etc.
 */
+
+//////////////////////////////
+// cart page
+// - add remove items
+// - add quantity change
+// - add total price calculation
+// - add checkout button w/whatsapp link
+
+
+//////////////////////////////
+// misc
+// - secure firebase firestore rules
+// - add rate limits
+// - add seo tags
+// - add meta description tags
+// - sitemap?
+// - top rated products in 404 page / redirect 404 to homepage
+
+/////////////////////////////
+// nice to have
+// - add stock/inventory count to products
+// - add out of stock label to products that have no inventory
+// - add the cycle loader lottie wherever applicable
+// - check code quality/optimization
